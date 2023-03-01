9c8df36e1ac5f7954393ae074b4687403c7c4d5b
Deekshith Ponugoti <deekshithponugoti@gmail.com>
2023-03-01 21:16:39 +0530

    first commit

--- backend/controllers/goalController.js
+++ backend/controllers/goalController.js
@@ -1,8 +1,12 @@
 const asyncHandler = require('express-async-handler');
+const Goal = require('../models/goalModel');
+const User = require('../models/userModel');
+
 
 
 const getGoals = asyncHandler(async (req, res) => {
-    res.status(200).json({message: 'Get Goals'})
+    const goals = await Goal.find({user: req.user._id});
+    res.status(200).json(goals);
 })
 
 const setGoals = asyncHandler(async (req, res) => {
@@ -10,15 +14,46 @@ const setGoals = asyncHandler(async (req, res) => {
         res.status(400)
         throw new Error('Text is required')
     }
-    res.status(200).json({message: 'Set Goals'})
+
+    const goal = await Goal.create({
+        text: req.body.text,
+        user: req.user._id,
+
+    })
+    res.status(200).json(goal)
 })
 
 const updateGoals = asyncHandler(async (req, res) => {
-    res.status(200).json({message: 'Update Goals'})
-})
+    const user = await User.findById(req.user.id);
+    if(!user){
+        res.status(401)
+        throw new Error('User not found')
+    }
+
+    if(goal.user.toString()!== user.id){
+        res.status(401)
+        throw new Error('User not authorized')
+    }
+    const goal = await Goal.findByIdAndUpdate(req.params.id, req.body, {
+        new: true,
+    })
+    res.status(200).json(goal)
+})  
 
 const deleteGoals = asyncHandler(async (req, res) => {
-    res.status(200).json({message: 'Delete Goals'})
+    const user = await User.findById(req.user.id);
+    if(!user){
+        res.status(401)
+        throw new Error('User not found')
+        
+    }
+    if(goal.user.toString()!== user.id){
+        res.status(401)
+        throw new Error('User not authorized')
+        
+    }
+    const goal = await Goal.findByIdAndDelete(req.params.id)
+    res.status(200).json(goal)
 })
 
 
--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,10 @@ const express = require('express')
 const dotenv = require('dotenv').config()
 const port = process.env.PORT || 5000
 const {errorHandler} = require('./middleware/errorMiddleware')
+const colors = require('colors')
+const connectDB = require('./config/db')
+
+connectDB()
 
 const app = express()
 
@@ -10,6 +14,8 @@ app.use(express.urlencoded({ extended: false}))
 
 
 app.use('/api/goals', require('./routes/goalRoutes'))
+app.use('/api/users', require('./routes/userRoutes'))
+
 app.use(errorHandler)
 
 app.listen(port, () => {
